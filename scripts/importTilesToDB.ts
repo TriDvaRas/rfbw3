121f4aa3100894908797e9d69f7e40225ca1b90b
TriDvaRas <02dimon02@gmail.com>
2023-06-25 23:11:24 +0300

    hotfixes

--- scripts/importTilesToDB.ts
+++ scripts/importTilesToDB.ts
@@ -1,14 +1,27 @@
 
-import data from "./tiles.json";
 
 import prisma from "../src/server/extra/lib/db";
+import fs from 'fs';
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const data: {
+    key: string,
+    childrenKeys: string[]
+    "buildings"?: string[],
+    "robots"?: string[],
+    "windows"?: string[]
+}[] = JSON.parse(fs.readFileSync("./scripts/tiles.json", "utf-8"));
 
 async function main() {
+    await prisma.tile.deleteMany({});
     await prisma.tile.createMany({
         data: data.map((tile) => {
             return {
                 location: tile.key,
-                connectedTo: tile.childrenKeys,
+                connectedTo: tile.childrenKeys || [],
+                buildings: tile.buildings || [],
+                robots: tile.robots || [],
+                windows: tile.windows || [],
             }
         })
     })
--- src/components/util/PlayerPreview.tsx
+++ src/components/util/PlayerPreview.tsx
@@ -18,8 +18,8 @@ function PlayerPreview({ motto, name, imageUrl, isUploading, progress, className
         <Avatar size={256} shape={'circle'} letters={getNameInitials(name)} src={imageUrl} className="text-7xl" />
         {isUploading && typeof progress == 'number' && <div className='absolute top-0 mask mask-circle w-full h-full bg-slate-900 bg-opacity-80 flex justify-center items-center'><RadialProgress value={progress}>{progress}%</RadialProgress></div>}
       </div>
-      <h3 className="text-3xl font-bold mt-2 mb-0">{name}</h3>
-      <h5 className="text-lg font-thin mt-0 mb-5">{motto}</h5>
+      <h3 className="text-3xl font-bold mt-2 mb-0 text-center">{name}</h3>
+      <h5 className="text-lg font-thin mt-0 mb-5 text-center">{motto}</h5>
     </div>
   )
 }
